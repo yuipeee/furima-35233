d55b69e7c8655311cb9cc9c1db4e0b8a343b4b02
yuipeee <nooof.0827@gmail.com>
2021-04-23 15:22:01 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,21 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one    :purchase
   has_one_attached :image
+
+  with_options presence: true do
+  validates :image
+  validates :name
+  validates :description
+  validates :price,         numericality: { greater_than_or_equal_to: 300, less_than: 9999999 },
+                            format: { /\A[0-9]+\z/ }
+  validates :user
+  end
+
+  with_options numericality: { other_than: 1 }  do
+  validates :category_id
+  validates :status_id
+  validates :shipping_cost_id
+  validates :shipping_area_id
+  end
+
 end
-
-
